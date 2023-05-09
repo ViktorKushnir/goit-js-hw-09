d2e8c1bfbf2b3d53467b9a9e0fde70e487cbf117
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-09 22:10:25 +0100

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+let intervalId = null;
+stopBtn.setAttribute('disabled', 'true');
+
+startBtn.addEventListener('click', clickStart);
+stopBtn.addEventListener('click', clickStop);
+
+function clickStart() {
+  intervalId= setInterval(()=>{
+    changeBgCollor()
+  }, 1000)
+
+  startBtn.setAttribute('disabled', 'true')
+  stopBtn.removeAttribute('disabled')
+}
+
+function clickStop(){
+    clearInterval(intervalId)
+    startBtn.removeAttribute('disabled')
+    stopBtn.setAttribute('disabled', 'true')
+}
+
+function changeBgCollor(){
+  const randomColor = getRandomHexColor()
+    document.body.style.backgroundColor = randomColor
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
