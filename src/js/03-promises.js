9655782b46c4ac9dc40b552d1fcdbce0df811c7b
ViktorKushnir <kushnirvv111@gmail.com>
2023-05-10 21:12:29 +0100

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix, { Notify } from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+formEl.addEventListener('submit', formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  let position = 1;
+  let nextDelay = Number(delay.value);
+
+  for (let i = 0; i < amount.value; i += 1) {
+    createPromise(position, nextDelay).then(({position, delay}) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+    })
+    .catch(({position, delay}) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+    })
+    position += 1;
+    nextDelay += Number(step.value)
   }
 }
